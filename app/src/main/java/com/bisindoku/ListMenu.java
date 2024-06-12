448e5dcf283cc55b56e0942b7303eefdbdce382c
Danu-Nur <diramadhan13@gmail.com>
2024-06-12 16:56:21 +0700

    list item

--- app/src/main/java/com/bisindoku/ListMenu.java
+++ app/src/main/java/com/bisindoku/ListMenu.java
@@ -1,6 +1,14 @@
 package com.bisindoku;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +16,69 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.bisindoku.databinding.ActivityListMenuBinding;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+
 public class ListMenu extends AppCompatActivity {
+
+    private ActivityListMenuBinding binding;
+    SearchView searchView;
+    ListView listView;
+    ArrayList<String> list;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_list_menu);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityListMenuBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Intent intent = getIntent();
+        String value = intent.getStringExtra("key");
+        list = new ArrayList<>();
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Pineapple");
+        list.add("Orange");
+        list.add("Lychee");
+        list.add("Gavava");
+        list.add("Peech");
+        list.add("Melon");
+        list.add("Watermelon");
+        list.add("Papaya");
+
+        adapter = new ArrayAdapter<>(this, R.layout.list_item, list);
+        binding.lv1.setAdapter(adapter);
+
+        // Change the divider color
+        binding.lv1.setDivider(new ColorDrawable(Color.BLACK));
+        binding.lv1.setDividerHeight(1);
+
+        binding.searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                if (list.contains(query)) {
+                    adapter.getFilter().filter(query);
+                } else {
+                    Snackbar.make(binding.lv1, "Item not found", Snackbar.LENGTH_SHORT).show();
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
         });
+
+//        EdgeToEdge.enable(this);
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
     }
 }
--- app/src/main/java/com/bisindoku/MenuBisindo.java
+++ app/src/main/java/com/bisindoku/MenuBisindo.java
@@ -1,6 +1,8 @@
 package com.bisindoku;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,33 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.bisindoku.databinding.ActivityMainBinding;
+import com.bisindoku.databinding.ActivityMenuBisindoBinding;
+
 public class MenuBisindo extends AppCompatActivity {
+    private ActivityMenuBisindoBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_menu_bisindo);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMenuBisindoBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnOrganTubuh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MenuBisindo.this, ListMenu.class);
+                intent.putExtra("key",1);
+                startActivity(intent);
+                finish();
+            }
         });
+//        EdgeToEdge.enable(this);
+//        setContentView(R.layout.activity_menu_bisindo);
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
     }
 }
