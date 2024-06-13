267a14994969bd868c7b0f5523bc5485412ab38d
Danu-Nur <diramadhan13@gmail.com>
2024-06-13 16:59:57 +0700

    activity play video

---
+++ app/src/main/java/com/bisindoku/PlayVideoActivity.java
@@ -0,0 +1,30 @@
+package com.bisindoku;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.bisindoku.databinding.ActivityPlayVideoBinding;
+
+public class PlayVideoActivity extends AppCompatActivity {
+
+    private ActivityPlayVideoBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityPlayVideoBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+//        EdgeToEdge.enable(this);
+//        setContentView(R.layout.activity_play_video);
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+    }
+}
